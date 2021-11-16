9094cb3d89b947e578f976d5d6c405cf836b5341
Grigore Stefan <g_stefan@yahoo.com>
2021-11-16 23:19:31 +0200

    Update

--- build/build.config.cmd
+++ build/build.config.cmd
@@ -4,5 +4,5 @@ rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 set PRODUCT_NAME=installer-nsis
-set PRODUCT_VERSION=3.06.1
+set PRODUCT_VERSION=3.08
 set PRODUCT_BASE=nsis
